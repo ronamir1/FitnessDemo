b43eeefd01c2eb9c5bee757c793bf25f57ff826c
sagi5362 <sagi.dekel@mail.huji.ac.il>
2020-08-25 18:56:57 +0300

    text to MuscleGroupActivity

--- app/src/main/java/com/example/fitnessdemo/MuscleGroupActivity.java
+++ app/src/main/java/com/example/fitnessdemo/MuscleGroupActivity.java
@@ -17,12 +17,19 @@ import java.util.Arrays;
 
 public class MuscleGroupActivity extends AppCompatActivity {
 
-    final String warmUpSet = "Warm up set 50% max weight 15 reps";
+    final String LINE_DROP = "\n";
+    final String firstOption = "First option:\n";
+    final String secondOption = "Second option (to vary training from time to time):\n";
+    final String warmUpSet = "Warm up set 50% max weight 15 reps.";
     final String firstSet = "First set 75% max weight 12 reps";
     final String secondSet = "Second set 85% max weight 10 reps";
     final String thirdSet = "Third and fourth set max weight 8 reps or failure (max you can do).";
-    final String classicSet = "Three sets of 95% max weight 12 reps or failure";
-    final String LINE_DROP = "\n";
+    final String classicSet = warmUpSet+LINE_DROP+"Three sets of 90% max weight 12 reps or failure";
+    final String raisingPyramide = "5 sets:"+LINE_DROP+warmUpSet+LINE_DROP+firstSet+LINE_DROP+secondSet+LINE_DROP+thirdSet;
+    final String bodyWeight = "4 sets overall.\nThe first is a warm up set, do half your maximum. On the other sets do the maximum you can.";
+    final String health = "This exercise require high rep so do three sets of 80% max weight, 15 reps.\nIf too hard drop to a weight that allows you this number of reps.";
+    final String combineClassicPyramide = firstOption+ raisingPyramide+LINE_DROP+LINE_DROP+secondOption+classicSet;
+    final String absClassic = "3 sets, slow movements, each set 30-45 seconds\nIf it is too hard, try to do your best.\nIf its easy peasy lemon squeezy you go ahead and do it for longer.";
     final static int CHEST = 0;
     final static int SHOULDERS = 1;
     final static int BACK = 2;
@@ -49,27 +56,27 @@ public class MuscleGroupActivity extends AppCompatActivity {
         switch(numMuscleGroup){
             case CHEST:
                 exercises = new String[]{"1. Bench press", "2. Dumbbell bench press ", "3. dips"};
-                info = new String[]{"5 sets:"+LINE_DROP+firstSet+LINE_DROP+secondSet+LINE_DROP+thirdSet, "You should", "You should"};
+                info = new String[]{raisingPyramide,raisingPyramide, bodyWeight};
                 break;
             case SHOULDERS:
                 exercises = new String[]{"1. Shoulder press"};
-                info = new String[]{"You should"};
+                info = new String[]{raisingPyramide};
                 break;
             case BACK:
                 exercises = new String[]{"1. Pull up", "2. Hip row", "3. Sitting row", "4. Pull over", " 5. One arm pull"};
-                info = new String[]{"You should", "You should", "You should", "You should", "You should"};
+                info = new String[]{bodyWeight, raisingPyramide, raisingPyramide, health, raisingPyramide};
                 break;
             case BICEPS:
                 exercises = new String[]{"1. Bicep curls", "2. French press"};
-                info = new String[]{"You should", "You should"};
+                info = new String[]{combineClassicPyramide, combineClassicPyramide};
                 break;
             case LEGS:
                 exercises = new String[]{"1. Squat", "2. Deadlift", "3. lunges", "4. Bulgarian split squat"};
-                info = new String[]{"You should", "You should", "You should", "You should"};
+                info = new String[]{raisingPyramide, raisingPyramide, combineClassicPyramide, combineClassicPyramide};
                 break;
             case ABS:
                 exercises = new String[]{"1. Static upper", "2. Accordion", "3. Legs raise", "4. Marine leg raise", "5. Bicycle", " 6. Side accordion"};
-                info = new String[]{"You should", "You should", "You should", "You should", "You should", "You should"};
+                info = new String[]{absClassic, absClassic, absClassic, absClassic, absClassic, absClassic};
                 break;
             default:
                 exercises = new String[]{};
