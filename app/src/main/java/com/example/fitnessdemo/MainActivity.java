e4ab9b0753225da5140b2465bbeea6bb54aed32a
sagi5362 <sagi.dekel@mail.huji.ac.il>
2020-09-12 12:52:05 +0300

    added more daily tips

--- app/src/main/java/com/example/fitnessdemo/MainActivity.java
+++ app/src/main/java/com/example/fitnessdemo/MainActivity.java
@@ -26,9 +26,21 @@ public class MainActivity extends AppCompatActivity {
     TextView dailyTip;
     final String DAILY_TIP = "Daily tip:";
     final String LINE_DROP = "\n";
-    String[] dailyTips = {"Drink at least 2 cups of water upon waking up in the morning",
-            "Eat at least 1 gram of protein for each kg you weigh\ni.e a 70kg men should eat at least 70 gram of protein a day",
-            "It is OK to take a day off to rest and gather strength for the next workout"};
+
+    final String tip1 = "Drink at least 3 liters of water every day";
+    final String tip2 = "Eat at least 1 gram of protein for each kg you weigh\ni.e a 70kg men should eat at least 70 gram of protein a day (don't forget to hydrate enough)";
+    final String tip3 = "It is OK to take a day off to rest and gather strength for the next workout";
+    final String tip4 = "Eating veggies and fruits is important, make sure you eat a few every day!";
+    final String tip5 = "Drinking black coffee and eating a banana or a date with some nut might improve your workout, try it!";
+    final String tip6 = "Sleeping is essential, make sure you get at least 7-8 hours of sleep every day";
+    final String tip7 = "Good posture and strong core can improve your deadlift, squat and many more exercises so don't neglect it";
+    final String tip8 = "Warm up before each workout to reduce risk of injury";
+    final String tip9 = "Nutrition menu should be given by a professional only!";
+    final String tip10 = "There is no such thing as local fat loss (i.e. only in your arms or abs)";
+    final String tip11 = "Stressed mind can cause a stressed body, make sure you relax sometimes";
+    final String tip12 = "Every exercise you are not sure about - ask your gym instructor they would be happy to help!";
+    final String tip13 = "Change does'nt come in a week, patience and consistency are key players";
+    String[] dailyTips = {tip1, tip2,tip3, tip4, tip5, tip6, tip7, tip8, tip9, tip10, tip11, tip12, tip13};
     Random rand = new Random();
 
     public void newHereActivity(MenuItem item){
