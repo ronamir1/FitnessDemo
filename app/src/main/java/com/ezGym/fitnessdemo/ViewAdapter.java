52cc848c05ad9ea04d134d1a5401848dc1033f7e
sagi5362 <sagi.dekel@mail.huji.ac.il>
2020-09-25 15:26:43 +0300

    changed \\n to \n

--- app/src/main/java/com/ezGym/fitnessdemo/ViewAdapter.java
+++ app/src/main/java/com/ezGym/fitnessdemo/ViewAdapter.java
@@ -14,13 +14,13 @@ public class ViewAdapter extends PagerAdapter {
 
     private Context context;
     private LayoutInflater layoutInflater;
-    final String tip1 = "No pain no gain is NOT true.\\nThere is a BIG difference between muscles hurting from a workout or from an injury. Listen to your body and rest if needed.";
+    final String tip1 = "No pain no gain is NOT true.\nThere is a BIG difference between muscles hurting from a workout or from an injury. Listen to your body and rest if needed.";
     final String tip2 = "Warm up is necessary, before each exercise do one slow set of 12-15 reps with lower weight.";
     final String tip3 = "Work on each body part (i.e. chest/shoulders/back/legs etc.) 1-2 times a week.";
-    final String tip4 = "The workouts are divided into 3 sections:\\nChest and shoulders, Back and Biceps/Triceps, Legs.\\nOn each workout, choose one of the sections and work it.\\nRegarding abs &amp; core, you can do them after each workout (no more than 3-4 times a week).";
+    final String tip4 = "The workouts are divided into 3 sections:\nChest and shoulders, Back and Biceps/Triceps, Legs.\nOn each workout, choose one of the sections and work it.\nRegarding abs &amp; core, you can do them after each workout (no more than 3-4 times a week).";
     final String tip5 = "your Max weight in an exercise is the maximum weight you can do 6-8 reps in that specific exercise.";
     final String tip6 = "Change your workout every few weeks, going in a rut in workouts is not good. Hence, you can either change the order of the exercises or choose different exercises, you have plenty of exercises to choose from in each section.";
-    final String tip7 = "Most important exercises to put effort into:\\nPull up, Bench press, Squat, Deadlift, Dips (this exercises increase testosterone levels).";
+    final String tip7 = "Most important exercises to put effort into:\nPull up, Bench press, Squat, Deadlift, Dips (this exercises increase testosterone levels).";
     final String tip8 = "Try to keep your core engaged in all of the exercises - this will stabilize you and help you achieve more quality reps.";
     private String[] strings = {tip1, tip2, tip3, tip4, tip5, tip6, tip7, tip8};
 
