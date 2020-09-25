7c17f962eeb1e5dc32c91ebb46be61fefc1672c1
sagi5362 <sagi.dekel@mail.huji.ac.il>
2020-09-25 15:22:32 +0300

    added circle indicator

--- app/src/main/java/com/ezGym/fitnessdemo/NewHereActivity.java
+++ app/src/main/java/com/ezGym/fitnessdemo/NewHereActivity.java
@@ -1,11 +1,15 @@
 package com.ezGym.fitnessdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+
+import me.relex.circleindicator.CircleIndicator;
 
 public class NewHereActivity extends AppCompatActivity {
 
@@ -15,9 +19,21 @@ public class NewHereActivity extends AppCompatActivity {
         startActivity(mainIntent);
     }
 
+    ViewPager viewPager2;
+    CircleIndicator circleIndicator;
+    ViewAdapter viewAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_here);
+
+        viewPager2 = (ViewPager) findViewById(R.id.viewPager2);
+        circleIndicator = (CircleIndicator) findViewById(R.id.indicator);
+
+
+        viewAdapter = new ViewAdapter(this);
+        viewPager2.setAdapter(viewAdapter);
+        circleIndicator.setViewPager(viewPager2);
     }
 }
---
+++ app/src/main/java/com/ezGym/fitnessdemo/ViewAdapter.java
@@ -0,0 +1,59 @@
+package com.ezGym.fitnessdemo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+public class ViewAdapter extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+    final String tip1 = "No pain no gain is NOT true.\\nThere is a BIG difference between muscles hurting from a workout or from an injury. Listen to your body and rest if needed.";
+    final String tip2 = "Warm up is necessary, before each exercise do one slow set of 12-15 reps with lower weight.";
+    final String tip3 = "Work on each body part (i.e. chest/shoulders/back/legs etc.) 1-2 times a week.";
+    final String tip4 = "The workouts are divided into 3 sections:\\nChest and shoulders, Back and Biceps/Triceps, Legs.\\nOn each workout, choose one of the sections and work it.\\nRegarding abs &amp; core, you can do them after each workout (no more than 3-4 times a week).";
+    final String tip5 = "your Max weight in an exercise is the maximum weight you can do 6-8 reps in that specific exercise.";
+    final String tip6 = "Change your workout every few weeks, going in a rut in workouts is not good. Hence, you can either change the order of the exercises or choose different exercises, you have plenty of exercises to choose from in each section.";
+    final String tip7 = "Most important exercises to put effort into:\\nPull up, Bench press, Squat, Deadlift, Dips (this exercises increase testosterone levels).";
+    final String tip8 = "Try to keep your core engaged in all of the exercises - this will stabilize you and help you achieve more quality reps.";
+    private String[] strings = {tip1, tip2, tip3, tip4, tip5, tip6, tip7, tip8};
+
+    ViewAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return strings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view== object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.item, null);
+        TextView textView = (TextView) view.findViewById(R.id.textView);
+        textView.setText(strings[position]);
+        ViewPager viewPager = (ViewPager) container;
+        viewPager.addView(view, 0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        ViewPager viewPager2 = (ViewPager) container;
+        View view = (View) object;
+        viewPager2.removeView(view);
+    }
+}
